4406f9557f82a5847c9e654e2e06b096182295f0
Homkai <wanghongkai@baidu.com>
2017-11-27 22:45:46 +0800

    update-cache

--- src/index.js
+++ src/index.js
@@ -5,11 +5,22 @@ import invariant from 'invariant';
 const cache = new Map();
 
 const setCache = (path, value) => {
-    cache.set(path, value);
+    path.reduce((level, key, index) => {
+        if (path.length - index > 1) {
+            !level.get(key) && level.set(key, new Map());
+            return level.get(key);
+        }
+        level.set(key, value);
+    }, cache);
 };
 
 const getCache = path => {
-    return cache.get(path);
+    return path.reduce((level, key) => {
+        if (!level || !level.has(key)) {
+            return;
+        }
+        return level.get(key);
+    }, cache);
 };
 /* utils end */
 
